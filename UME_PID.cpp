694a05210e217ec8e3a71a1ab27f645bde4355b3
Bison <33536600+Bison0522@users.noreply.github.com>
2019-05-09 10:15:07 +0900

    Update UME_PID.cpp

--- UME_PID.cpp
+++ UME_PID.cpp
@@ -1,7 +1,29 @@
+// pid.h
+#pragma once
+class pid
+{
+private:
+	double Kp;
+	double Ki;
+	double Kd;
+	double En;
+	double En_1;
+	double En_2;
+	double b;
+	double integ;
+
+public:
+	pid(double, double, double);
+
+	double get_pid(double, double);
+
+	~pid();
+};
+
+/////////////////////////////////////////////
+
 #include "pid.h"
 #include <stdio.h>
-
-
 
 pid::pid( double p, double i, double d ) {
 	Kp = p;
